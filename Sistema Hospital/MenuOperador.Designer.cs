1c0a78575d5266efef0f8dbdc35c0f117af409bc
Fernando <Fernando@DESKTOP-B4854MF>
2025-06-08 14:59:36 -0400

    Busqueda de Citas hecha

--- Sistema Hospital/MenuOperador.Designer.cs
+++ Sistema Hospital/MenuOperador.Designer.cs
@@ -46,117 +46,165 @@
             this.btnGestionarPermisos = new System.Windows.Forms.Button();
             this.btnVerDisponibilidad = new System.Windows.Forms.Button();
             this.panelDerecho = new System.Windows.Forms.Panel();
+            this.lblRut = new System.Windows.Forms.Label();
             this.txtBuscarRut = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
             this.dtpBuscarFecha = new System.Windows.Forms.DateTimePicker();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.dgvCitas = new System.Windows.Forms.DataGridView();
-            this.lblRut = new System.Windows.Forms.Label();
-            this.lblFecha = new System.Windows.Forms.Label();
-
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.panelIzquierdo.SuspendLayout();
+            this.panelDerecho.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).BeginInit();
+            this.SuspendLayout();
             // 
             // panelIzquierdo
             // 
+            this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(221)))), ((int)(((byte)(187)))));
             this.panelIzquierdo.Controls.Add(this.btnAgendarCita);
             this.panelIzquierdo.Controls.Add(this.btnGenerarInforme);
             this.panelIzquierdo.Controls.Add(this.btnGestionarPermisos);
             this.panelIzquierdo.Controls.Add(this.btnVerDisponibilidad);
             this.panelIzquierdo.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelIzquierdo.Width = 180;
-            this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(102, 221, 187);
-
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(180, 448);
+            this.panelIzquierdo.TabIndex = 1;
             // 
-            // Botones
+            // btnAgendarCita
             // 
+            this.btnAgendarCita.Location = new System.Drawing.Point(15, 30);
+            this.btnAgendarCita.Name = "btnAgendarCita";
+            this.btnAgendarCita.Size = new System.Drawing.Size(150, 23);
+            this.btnAgendarCita.TabIndex = 0;
             this.btnAgendarCita.Text = "Agendar Cita";
-            this.btnAgendarCita.Top = 30;
-            this.btnAgendarCita.Width = 150;
-            this.btnAgendarCita.Left = 15;
-
+            // 
+            // btnGenerarInforme
+            // 
+            this.btnGenerarInforme.Location = new System.Drawing.Point(15, 80);
+            this.btnGenerarInforme.Name = "btnGenerarInforme";
+            this.btnGenerarInforme.Size = new System.Drawing.Size(150, 23);
+            this.btnGenerarInforme.TabIndex = 1;
             this.btnGenerarInforme.Text = "Generar Informe";
-            this.btnGenerarInforme.Top = 80;
-            this.btnGenerarInforme.Width = 150;
-            this.btnGenerarInforme.Left = 15;
-
+            // 
+            // btnGestionarPermisos
+            // 
+            this.btnGestionarPermisos.Location = new System.Drawing.Point(15, 130);
+            this.btnGestionarPermisos.Name = "btnGestionarPermisos";
+            this.btnGestionarPermisos.Size = new System.Drawing.Size(150, 23);
+            this.btnGestionarPermisos.TabIndex = 2;
             this.btnGestionarPermisos.Text = "Gestionar Permisos";
-            this.btnGestionarPermisos.Top = 130;
-            this.btnGestionarPermisos.Width = 150;
-            this.btnGestionarPermisos.Left = 15;
-
+            // 
+            // btnVerDisponibilidad
+            // 
+            this.btnVerDisponibilidad.Location = new System.Drawing.Point(15, 180);
+            this.btnVerDisponibilidad.Name = "btnVerDisponibilidad";
+            this.btnVerDisponibilidad.Size = new System.Drawing.Size(150, 23);
+            this.btnVerDisponibilidad.TabIndex = 3;
             this.btnVerDisponibilidad.Text = "Ver Disponibilidad";
-            this.btnVerDisponibilidad.Top = 180;
-            this.btnVerDisponibilidad.Width = 150;
-            this.btnVerDisponibilidad.Left = 15;
-
             // 
             // panelDerecho
             // 
-            this.panelDerecho.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDerecho.BackColor = System.Drawing.Color.White;
+            this.panelDerecho.Controls.Add(this.btnLimpiar);
             this.panelDerecho.Controls.Add(this.lblRut);
             this.panelDerecho.Controls.Add(this.txtBuscarRut);
             this.panelDerecho.Controls.Add(this.lblFecha);
             this.panelDerecho.Controls.Add(this.dtpBuscarFecha);
             this.panelDerecho.Controls.Add(this.btnBuscar);
             this.panelDerecho.Controls.Add(this.dgvCitas);
-            this.panelDerecho.BackColor = System.Drawing.Color.White;
-
+            this.panelDerecho.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDerecho.Location = new System.Drawing.Point(180, 0);
+            this.panelDerecho.Name = "panelDerecho";
+            this.panelDerecho.Size = new System.Drawing.Size(608, 448);
+            this.panelDerecho.TabIndex = 0;
             // 
             // lblRut
             // 
+            this.lblRut.Location = new System.Drawing.Point(20, 20);
+            this.lblRut.Name = "lblRut";
+            this.lblRut.Size = new System.Drawing.Size(40, 23);
+            this.lblRut.TabIndex = 0;
             this.lblRut.Text = "RUT:";
-            this.lblRut.Top = 20;
-            this.lblRut.Left = 20;
-            this.lblRut.Width = 40;
-
             // 
             // txtBuscarRut
             // 
-            this.txtBuscarRut.Top = 18;
-            this.txtBuscarRut.Left = 65;
-            this.txtBuscarRut.Width = 120;
-
+            this.txtBuscarRut.Location = new System.Drawing.Point(65, 18);
+            this.txtBuscarRut.MinimumSize = new System.Drawing.Size(0, 10);
+            this.txtBuscarRut.Name = "txtBuscarRut";
+            this.txtBuscarRut.Size = new System.Drawing.Size(120, 20);
+            this.txtBuscarRut.TabIndex = 1;
+            this.txtBuscarRut.TextChanged += new System.EventHandler(this.txtBuscarRut_TextChanged);
             // 
             // lblFecha
             // 
+            this.lblFecha.Location = new System.Drawing.Point(200, 20);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(50, 23);
+            this.lblFecha.TabIndex = 2;
             this.lblFecha.Text = "Fecha:";
-            this.lblFecha.Top = 20;
-            this.lblFecha.Left = 200;
-            this.lblFecha.Width = 50;
-
             // 
             // dtpBuscarFecha
             // 
-            this.dtpBuscarFecha.Top = 18;
-            this.dtpBuscarFecha.Left = 255;
-            this.dtpBuscarFecha.Width = 120;
             this.dtpBuscarFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-
+            this.dtpBuscarFecha.Location = new System.Drawing.Point(255, 18);
+            this.dtpBuscarFecha.Name = "dtpBuscarFecha";
+            this.dtpBuscarFecha.Size = new System.Drawing.Size(120, 20);
+            this.dtpBuscarFecha.TabIndex = 3;
             // 
             // btnBuscar
             // 
+            this.btnBuscar.Location = new System.Drawing.Point(390, 16);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(80, 23);
+            this.btnBuscar.TabIndex = 4;
             this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.Top = 16;
-            this.btnBuscar.Left = 390;
-            this.btnBuscar.Width = 80;
-
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
             // 
             // dgvCitas
             // 
-            this.dgvCitas.Top = 60;
-            this.dgvCitas.Left = 20;
-            this.dgvCitas.Width = 600;
-            this.dgvCitas.Height = 350;
-            this.dgvCitas.Anchor = (System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right | System.Windows.Forms.AnchorStyles.Bottom);
+            this.dgvCitas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvCitas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-
+            this.dgvCitas.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvCitas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvCitas.Location = new System.Drawing.Point(23, 58);
+            this.dgvCitas.Name = "dgvCitas";
+            this.dgvCitas.ReadOnly = true;
+            this.dgvCitas.Size = new System.Drawing.Size(582, 387);
+            this.dgvCitas.TabIndex = 5;
+            this.dgvCitas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCitas_CellContentClick);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(486, 15);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(83, 24);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar Filtro";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.button1_Click);
             // 
             // MenuOperador
             // 
+            this.ClientSize = new System.Drawing.Size(788, 448);
             this.Controls.Add(this.panelDerecho);
             this.Controls.Add(this.panelIzquierdo);
+            this.Name = "MenuOperador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menú Operador";
-            this.Size = new System.Drawing.Size(850, 450);
+            this.Load += new System.EventHandler(this.MenuOperador_Load);
+            this.panelIzquierdo.ResumeLayout(false);
+            this.panelDerecho.ResumeLayout(false);
+            this.panelDerecho.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
--- Sistema Hospital/MenuOperador.cs
+++ Sistema Hospital/MenuOperador.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
 
 namespace Sistema_Hospital
 {
     public partial class MenuOperador : Form
     {
+        string connectionString = "server=localhost;user=root;password=;database=centroclinico;port=3306;sslmode=none;";
+        string rut;
+        DateTime fecha;
         public MenuOperador()
         {
             InitializeComponent();
@@ -26,6 +31,115 @@ namespace Sistema_Hospital
             // para filtrar las citas por RUT y fecha, y luego mostrar los resultados en dgvCitas.
             // Ejemplo:
             // dgvCitas.DataSource = ObtenerCitasFiltradas(rut, fecha);
+        }
+
+        private void dgvCitas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void MenuOperador_Load(object sender, EventArgs e)
+        {
+            CargarCitas();
+        }
+        private void CargarCitas()
+        {
+            try
+            {
+                
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT RutPaciente,IdMedico,FechaHora,Estado,NotasMedicas FROM cita;"; // Asegúrate de que la tabla 'citas' exista en tu base de datos
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                        DataTable citasTable = new DataTable();
+                        adapter.Fill(citasTable);
+                        dgvCitas.DataSource = citasTable;
+                    }
+                    connection.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error al cargar las citas: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            rut = txtBuscarRut.Text.Trim();
+            fecha = dtpBuscarFecha.Value.Date;
+
+            if (string.IsNullOrEmpty(rut))
+            {
+                MessageBox.Show("Por favor, ingrese un RUT válido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    //abrir conexión
+                    conn.Open();
+                    //filtrar tabla por rut
+                    string query = "SELECT RutPaciente,IdMedico,FechaHora,Estado,NotasMedicas from cita where RutPaciente = @rut and DATE(FechaHora) >= @fecha;";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@rut", rut);
+                        cmd.Parameters.AddWithValue("@fecha", fecha);
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        DataTable citasFiltradas = new DataTable();
+                        adapter.Fill(citasFiltradas);
+                        if (citasFiltradas.Rows.Count > 0)
+                        {
+                            dgvCitas.DataSource = citasFiltradas;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se encontraron citas para el RUT y la fecha especificados.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            dgvCitas.DataSource = null; // Limpiar el DataGridView si no hay resultados
+                        }
+                    }
+                }
+            }
+        }
+
+        private void txtBuscarRut_TextChanged(object sender, EventArgs e)
+        {
+            string texto = txtBuscarRut.Text.Replace("-", "").Trim(); //ELIMINA UIONES Y ESPACIOS DEL RUT
+            if (texto.Length > 1)
+            {
+                string cuerpo = texto.Substring(0, texto.Length - 1); // Obtiene el cuerpo del RUT sin el dígito verificador
+                string dv = texto.Substring(texto.Length - 1, 1); // Obtiene el dígito verificador del RUT
+                txtBuscarRut.Text = cuerpo + "-" + dv; // Vuelve a construir el RUT con el guion
+                txtBuscarRut.SelectionStart = txtBuscarRut.Text.Length; // Mueve el cursor al final del texto
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBuscarRut.ResetText(); // Resetea el campo de búsqueda de RUT
+            dtpBuscarFecha.Value = DateTime.Now; // Resetea la fecha al día actual
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                //abrir conexión
+                conn.Open();
+                string query = "SELECT RutPaciente,IdMedico,FechaHora,Estado,NotasMedicas from cita;"; // Consulta para obtener todas las citas
+                using (MySqlCommand cmd = new MySqlCommand(query, conn)) // Ejecuta la consulta para obtener todas las citas
+                {
+                    
+
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd); // Crea un adaptador para llenar el DataTable
+                    DataTable citasTable = new DataTable(); // Crea un DataTable para almacenar los resultados
+                    adapter.Fill(citasTable); // Llena el DataTable con los resultados de la consulta
+                    dgvCitas.DataSource = citasTable; // Asigna el DataTable al DataGridView para mostrar todas las citas
+
+                }
+            }
+
         }
     }
 }
